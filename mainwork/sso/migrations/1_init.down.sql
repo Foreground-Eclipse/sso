009b3c420475c1007c1d517215ddf7c353e1ebce
Foreground-Eclipse <esdeathgodd@gmail.com>
2024-06-18 01:06:15 +0700

    Delete mainwork/sso directory

--- mainwork/sso/migrations/1_init.down.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS appsecrets;
-drop table if exists AccountConfirmations;
-drop table if exists ExternalAuth;
-drop table if exists PasswordResets;
-drop table if exists Sessions;
